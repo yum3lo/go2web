3f88de2295c83f47c61aea116a97f2dd6a1bcae1
maria <118808220+yum3lo@users.noreply.github.com>
2025-03-22 17:43:26 +0200

    Create search functionality

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
 import argparse
 import socket
+import ssl
 
 def fetch_url(url):
   try:
-    if not url.startswith('http://'):
+    if not url.startswith(('http://', 'https://')):
       url = 'http://' + url
+
+    protocol = 'http'
+    if url.startswith('https://'):
+      protocol = 'https'
 
     host = url.split('/')[2]
     path = '/' + '/'.join(url.split('/')[3:])
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, 80))
+    # Wrapping the socket with SSL if HTTPS
+    if protocol == 'https':
+      context = ssl.create_default_context()
+      # Forcing TLSv1.2 or higher for the specific SSL/TLS version DuckDuckGo API requires
+      context.minimum_version = ssl.TLSVersion.TLSv1_2
+      context.maximum_version = ssl.TLSVersion.TLSv1_3
+      s = context.wrap_socket(s, server_hostname=host)
+      port = 443
+    else:
+      port = 80
+            
+    s.connect((host, port))
 
     request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
     s.sendall(request.encode())
@@ -28,6 +44,11 @@ def fetch_url(url):
   except Exception as e:
     return f"Error: {str(e)}"
 
+def search_web(query):
+  search_url = f"https://api.duckduckgo.com/?q={query}&format=json"
+  response = fetch_url(search_url)
+  return response
+
 def main():
   parser = argparse.ArgumentParser(description="CLI tool for PWeb lab 5")
   parser.add_argument('-u', '--url', help='Make an HTTP request to the specified URL and print the response')
@@ -38,7 +59,8 @@ def main():
     response = fetch_url(args.url)
     print(response)
   elif args.search:
-    print(f'Searching for {args.search} using Google')
+    response = search_web(args.search)
+    print(response)
   else:
     parser.print_help()
 
