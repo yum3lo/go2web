82e56b7c8e6421d9d57370403200a3f9c6995148
maria <118808220+yum3lo@users.noreply.github.com>
2025-03-22 17:03:37 +0200

    Create HTTP request functionality

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
 import argparse
+import socket
+
+def fetch_url(url):
+  try:
+    if not url.startswith('http://'):
+      url = 'http://' + url
+
+    host = url.split('/')[2]
+    path = '/' + '/'.join(url.split('/')[3:])
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, 80))
+
+    request = f"GET {path} HTTP/1.1\r\nHost: {host}\r\nConnection: close\r\n\r\n"
+    s.sendall(request.encode())
+
+    response = b""
+    while True:
+      data = s.recv(4096)
+      if not data:
+        break
+      response += data
+
+    s.close()
+
+    return response.decode('utf-8', errors='ignore')
+  except Exception as e:
+    return f"Error: {str(e)}"
 
 def main():
   parser = argparse.ArgumentParser(description="CLI tool for PWeb lab 5")
@@ -7,7 +35,8 @@ def main():
   
   args = parser.parse_args()
   if args.url:
-    print(f'Making an HTTP request to {args.url}')
+    response = fetch_url(args.url)
+    print(response)
   elif args.search:
     print(f'Searching for {args.search} using Google')
   else:
